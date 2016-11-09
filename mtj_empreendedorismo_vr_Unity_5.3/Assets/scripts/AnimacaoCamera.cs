df67ad9a506e21f072ee9542924d3c3742178305
Ricardo Luiz <ricardoluiz508@gmail.com>
2016-11-08 23:13:20 -0200

    Adicionado animações e luz a cena
    foi inserido animações básicas para a camera e coloca iluminação nas
    lampadas. Retirado a texturização por problemas na iluminação

---
+++ mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/AnimacaoCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimacaoCamera : MonoBehaviour {
+
+	public Animator controladorAnimacao;
+
+	void Start () {
+		controladorAnimacao.enabled = true;
+		controladorAnimacao.Play ("camaToLeptop");
+	}		
+
+}
---
+++ mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/MenuMural.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMural : MonoBehaviour {
+
+	private Animator mainCameraAnimator;
+	private bool mural = false;
+	private bool lepTop = false;
+	private bool porta = false;
+
+	void Start () {
+		mainCameraAnimator = Camera.main.gameObject.GetComponent<Animator> (); //Pegar animator da camera principal
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Função para realizar a função da camera ir ate o mural
+	public void trazerCameraAteMural(){		
+		resetarStatus ();
+		this.mural = true;
+		mainCameraAnimator.enabled = true;
+		mainCameraAnimator.Play ("camaToMural");
+	}
+
+	//Função para realizar a função da camera ir ate o LepTop
+	public void trazerCameraAteLepTop(){
+		resetarStatus ();
+		this.lepTop = true;
+		mainCameraAnimator.enabled = true;
+		mainCameraAnimator.Play ("camaToLeptop");
+	}
+
+	//Função para realizar a função da camera ir ate a porta
+	public void trazerCameraAtePorta(){
+		mainCameraAnimator.enabled = true;
+		mainCameraAnimator.Play ("camaToPorta");
+	}
+
+	//Função para realizar a função da camera ir do Mural para a Cama
+	public void trazerCameraAteMuralToCama(){
+		if(this.mural){
+			mainCameraAnimator.enabled = true;
+			mainCameraAnimator.Play ("muralToCama");
+		}
+	}
+
+	//Função para realizar a função da camera ir do LepTop para a Cama
+	public void trazerCameraAteLeptopToCama(){
+		if(this.lepTop){
+			mainCameraAnimator.enabled = true;
+			mainCameraAnimator.Play ("leptopToCama");
+		}
+	}
+
+	//Reseta os Status de onde a camera está
+	void resetarStatus(){
+		this.mural = false;
+		this.lepTop = false;
+		this.porta = false;
+	}
+}
