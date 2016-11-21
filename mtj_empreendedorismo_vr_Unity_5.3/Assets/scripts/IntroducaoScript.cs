563017f7fc8da149363b6060a9595708dddc8c63
claraabandeira <clara-alice@hotmail.com>
2016-11-21 19:51:05 -0300

    Botão Start funcionando (com roteiro)
    Falta os outros elementos do menu e a segunda parte do roteiro.

---
+++ mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/IntroducaoScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroducaoScript : MonoBehaviour {
+
+    public AudioClip sound;
+    public float volume;
+    AudioSource audio;
+    public bool soundplayed = false;
+    void Start() {
+        audio = GetComponent<AudioSource>();
+    }
+   public void OnEnterTrigger() {
+        if (!soundplayed) {
+            audio.PlayOneShot(sound, volume);
+            soundplayed = true;
+        }
+    }
+}
