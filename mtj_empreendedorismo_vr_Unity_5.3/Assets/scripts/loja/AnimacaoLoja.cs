dbfadc9cba4fa99f8a7786782333746216e58ca9
Ricardo Luiz <ricardoluiz508@gmail.com>
2016-12-11 01:40:29 -0200

    Adicionado loja
    Adicionado modelo da loja junto com a animação de abrir portas no unity.
    Criado nova cena para para loja. Ainda falta linkar o quarto com a loja
    e vice versa.

---
+++ mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/loja/AnimacaoLoja.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimacaoLoja : MonoBehaviour {
+
+	public Animator animatorLoja;
+	private Animator animatorCamera;
+
+	void Start () {
+		animatorCamera = Camera.main.gameObject.GetComponent<Animator> ();
+		//animacao.Play ("porta1");
+	}
+
+	//Faz o jogador entrar na loja. Ativa animações da camera e da loja
+	public void entrarLoja(){
+		animatorLoja.enabled = true;
+		animatorCamera.enabled = true;
+		animatorCamera.Play ("EntrarLoja");
+		animatorLoja.Play ("porta1");
+	}
+
+	public void verCarrinho(){
+		animatorCamera.enabled = true;
+		animatorCamera.Play ("VerCarrinho");
+	}
+}
