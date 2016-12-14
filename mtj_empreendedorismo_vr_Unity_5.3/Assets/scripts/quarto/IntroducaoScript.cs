a87ee0102e83300f4698133cf04fd3a36b6606e7
claraabandeira <clara-alice@hotmail.com>
2016-12-13 22:34:41 -0300

    Opções feitas (sem a voz feminina)
    não consegui fazer o script pra voz feminina

--- mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/quarto/IntroducaoScript.cs
+++ mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/quarto/IntroducaoScript.cs
@@ -3,31 +3,61 @@ using System.Collections;
 
 public class IntroducaoScript : MonoBehaviour {
 
-    public AudioClip sound1; //Audio do primeiro roteiro
-	public AudioClip sound2; //Audio do segundo roteiro
+    public AudioClip sound1F;//Audio do primeiro roteiro feminino
+    public AudioClip sound1M;//Audio do primeiro roteiro masculino
+    public AudioClip sound2F; //Audio do segundo roteiro feminino
+    public AudioClip sound2M; //Audio do segundo roteiro masculino
 
     public float volume;
     AudioSource audio;
-    bool soundplayed1 = false; //tocou o roteiro 1?
-	bool soundplayed2 = false; //tocou o roteiro 2?
-    
-	void Start() {
+    bool soundplayed1F = false; //tocou o roteiro 1 (feminino)?
+    bool soundplayed2F = false; //tocou o roteiro 2 (feminino)?
+    bool soundplayed1M = false; //tocou o roteiro 1 (masculino)?
+    bool soundplayed2M = false; //tocou o roteiro 2 (masculino)?
+    bool clickfeminino = false;
+    void Start() {
 		audio = Camera.main.gameObject.GetComponent<AudioSource>();
     }
-
-	//Audio corresponde ao primeiro Roteiro. "Que legal ganhei dinheiro de preset..."
-   	public void IniciarAudio() {
-        if (!soundplayed1) {
-            audio.PlayOneShot(sound1, volume);
-            soundplayed1 = true;
+    public void TriggerFeminino() {
+       
+    }
+    public void IniciarAudioF() {
+        if (clickfeminino == true)
+        {
+            if (!soundplayed1F)
+            {
+                audio.PlayOneShot(sound1F, volume);
+                soundplayed1F = true;
+            }
+        }
+        else
+        {
+            if (!soundplayed1M)
+            {
+                audio.PlayOneShot(sound1M, volume);
+                soundplayed1M = true;
+            }
+        }
+       }
+    //Audio corresponde ao segundo Roteiro. "Vou investir esse dinheiro..."
+    public void IniciarAudio2() {
+        if (clickfeminino == true)
+        {
+            if (!soundplayed2F)
+            {
+                audio.PlayOneShot(sound2F, volume);
+                soundplayed2F = true;
+            }
+        }
+        else
+        {
+            if (!soundplayed2M)
+            {
+                audio.PlayOneShot(sound2M, volume);
+                soundplayed2M = true;
+            }
         }
     }
 
-	//Audio corresponde ao segundo Roteiro. "Vou investir esse dinheiro..."
-	public void IniciarAudio2(){
-		if (!soundplayed2 && soundplayed1) {
-			audio.PlayOneShot(sound2, volume);
-			soundplayed2 = true;
-		}
-	}
-}
+    }
+
