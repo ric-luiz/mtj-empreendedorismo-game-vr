cbbb255c508e1c40d4ed7ada726a006a8464457b
Ricardo Luiz <ricardoluiz508@gmail.com>
2016-12-14 00:51:09 -0200

    Inserido Placas para voltar na cena da loja
    As placas para voltar do carrinho para o inicio da loja e a placa para
    voltar pro quarto.

--- mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/loja/AnimacaoLoja.cs
+++ mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/loja/AnimacaoLoja.cs
@@ -1,26 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class AnimacaoLoja : MonoBehaviour {
+	//placas de voltar a serem ativadas e desativadas
+	public GameObject placaVoltarCarrinho;
 
+	//Animator dos objetos
 	public Animator animatorLoja;
 	private Animator animatorCamera;
 
 	void Start () {
 		animatorCamera = Camera.main.gameObject.GetComponent<Animator> ();
-		//animacao.Play ("porta1");
 	}
 
 	//Faz o jogador entrar na loja. Ativa animações da camera e da loja
 	public void entrarLoja(){
-		animatorLoja.enabled = true;
-		animatorCamera.enabled = true;
+		habilitarAnimatorLoja ();
+		habilitarAnimatorCamera();
 		animatorCamera.Play ("EntrarLoja");
 		animatorLoja.Play ("porta1");
 	}
 
+	//Faz o Personagem chegar ate o carrinho
 	public void verCarrinho(){
-		animatorCamera.enabled = true;
+		habilitarAnimatorCamera();
+		placaVoltarCarrinho.SetActive(true);
 		animatorCamera.Play ("VerCarrinho");
 	}
+
+	// faz a animação para o jogador voltar para o inicio da loja
+	public void voltarDoCarrinho(){
+		habilitarAnimatorCamera();
+		placaVoltarCarrinho.SetActive(false);
+		animatorCamera.Play ("VoltarDoCarrinho");
+	}
+
+	public void voltarQuarto(){
+		SceneManager.LoadScene ("quarto",LoadSceneMode.Single);
+	}
+
+	private void habilitarAnimatorCamera(){
+		animatorCamera.enabled = true;
+	}
+
+	private void habilitarAnimatorLoja(){
+		animatorLoja.enabled = true;
+	}		
 }
