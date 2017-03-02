f771905033589dfb71b13b354b5889032b322fc6
ricardo luiz <ricardoluiz508@gmail.com>
2017-03-02 18:03:31 -0300

    Removido arquivo
    Foi removido arquivos duplicados do projeto

--- mtj_empreendedorismo_vr_Unity_5.3/mtj_empreendedorismo_vr_Unity_5.3/Assets/scripts/VoltarLimbo.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
-
-public class VoltarLimbo : MonoBehaviour {
-	public void Quarto(){
-		SceneManager.LoadScene ("quarto",LoadSceneMode.Single);
-	}
-}
